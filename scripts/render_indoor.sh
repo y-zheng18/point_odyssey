df72cf3c2026377369b1316da7f098f2f44328cc
y-zheng18 <yzheng18@stanford.edu>
2024-10-20 12:54:29 -0700

    update rendering script

--- scripts/render_indoor.sh
+++ scripts/render_indoor.sh
@@ -2,4 +2,4 @@
 
 python export_annotation.py --scene_dir ./data/demo_scene/kitchen_gfloor.blend --save_dir ./results/indoor --rendering --samples_per_pixel 128 --add_fog --randomize \
 	--exr --export_obj \
-	--export_tracking --vis_num 200 --sampling_character_num 5000 --sampling_scene_num 2000
+	--export_tracking --sampling_character_num 5000 --sampling_scene_num 2000
--- scripts/render_robot.sh
+++ scripts/render_robot.sh
@@ -2,4 +2,4 @@
 
 python export_annotation.py --scene_dir ./data/demo_scene/robot.blend --save_dir ./results/robot --rendering --samples_per_pixel 64  \
 	--exr --export_obj \
-	--export_tracking --vis_num 200 --sampling_character_num 5000 --sampling_scene_num 2000
+	--export_tracking --sampling_character_num 5000 --sampling_scene_num 2000
